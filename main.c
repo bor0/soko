e07e0967d1ff601e1940f2982c5745b13d9e7d0b
div0man <44116802+div0man@users.noreply.github.com>
2018-11-01 10:33:09 +0100

    Add feature to specify the level as command-line argument

--- main.c
+++ main.c
@@ -22,17 +22,25 @@ along with Soko. If not, see <http://www.gnu.org/licenses/>.
 #include "level.h"
 #include "soko_sdl.h"
 
-int main(void) {
+#define LEVELNAMESZ 512
+
+int main(int argc, char *argv[]) {
     FILE *t;
     struct level *map;
-    char level_name[512];
+    char level_name[LEVELNAMESZ];
     int asset_width = 32, asset_height = 32, i;
 
-    printf("Soko v1.0 by Boro Sitnikovski\n=============================\nEnter a valid level name (levels/*.dat): ");
+    printf("Soko v1.0 by Boro Sitnikovski\n=============================\n");
 
-    if (fgets(level_name, 512, stdin) == NULL) {
-        printf("Enter a valid level, m8.");
-        return 0;
+    if (argc > 1) {
+        strncpy(level_name, argv[1], LEVELNAMESZ-1);
+    }
+    else {
+        printf("Enter a valid level name (levels/*.dat): ");
+        if (fgets(level_name, 512, stdin) == NULL) {
+            printf("Enter a valid level, m8.\n");
+            return 0;
+        }
     }
 
     /* remove newlines */
